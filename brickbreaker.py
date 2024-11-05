d3ebf8de2b6280911768de5cb00b2bd2de5e30fa
Victor Kipngetich <victor.kipngetich@strathmore.edu>
2024-11-05 05:30:47 +0300

    add ball and paddle class

--- brickbreaker.py
+++ brickbreaker.py
@@ -13,3 +13,44 @@ PADDLE_HEIGHT = 15
 BALL_RADIUS = 10
 
 LIVES_FONT = pygame.font.SysFont("comicsans", 40)
+
+class Paddle:
+    VEL = 5
+
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+
+    def move(self, direction=1):
+        self.x += self.VEL * direction
+
+class Ball:
+    VEL = 5
+
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.x_vel = 0
+        self.y_vel = -self.VEL
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+    def set_vel(self, x_vel, y_vel):
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+
+    def draw(self, win):
+        # Draw highlight for a 3D effect
+        highlight_color = (255, 255, 255)  # White highlight
+        pygame.draw.circle(win, highlight_color, (self.x + 3, self.y + 3), self.radius)  # Highlight
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)  # Main color
